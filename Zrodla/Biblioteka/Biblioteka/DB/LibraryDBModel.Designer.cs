ab85faf00caac8da8faf68a87a961fa0da0f8825
marcjan940 <marcjan940@student.polsl.pl>
2016-05-05 20:22:02 +0200

    removed packages

--- Zrodla/Biblioteka/Biblioteka/DB/LibraryDBModel.Designer.cs
+++ Zrodla/Biblioteka/Biblioteka/DB/LibraryDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Kuba\Desktop\Pliki\BD projekt\BD projekt\biblioteka\Zrodla\Biblioteka\Biblioteka\DB\LibraryDBModel.edmx'. 
+﻿// T4 code generation is enabled for model
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
