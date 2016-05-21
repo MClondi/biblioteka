95d61cf6de8f37b0c77f49e621008342bb68c37c
Kuba Plachta <jplachta94@gmail.com>
2016-05-21 13:44:57 +0200

    Admin panel added

---
+++ Zrodla/Biblioteka/Biblioteka/Forms/AdminForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Biblioteka.Forms
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.lstViewAllUsers = new System.Windows.Forms.ListView();
+            this.btnEditUser = new System.Windows.Forms.Button();
+            this.btnDeleteUser = new System.Windows.Forms.Button();
+            this.btnSearchUser = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(12, 261);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(112, 23);
+            this.btnAddUser.TabIndex = 0;
+            this.btnAddUser.Text = "Dodaj użytkownika";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            // 
+            // lstViewAllUsers
+            // 
+            this.lstViewAllUsers.Location = new System.Drawing.Point(12, 51);
+            this.lstViewAllUsers.Name = "lstViewAllUsers";
+            this.lstViewAllUsers.Size = new System.Drawing.Size(466, 186);
+            this.lstViewAllUsers.TabIndex = 1;
+            this.lstViewAllUsers.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnEditUser
+            // 
+            this.btnEditUser.Location = new System.Drawing.Point(130, 261);
+            this.btnEditUser.Name = "btnEditUser";
+            this.btnEditUser.Size = new System.Drawing.Size(112, 23);
+            this.btnEditUser.TabIndex = 2;
+            this.btnEditUser.Text = "Edytuj użytkownika";
+            this.btnEditUser.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteUser
+            // 
+            this.btnDeleteUser.Location = new System.Drawing.Point(248, 261);
+            this.btnDeleteUser.Name = "btnDeleteUser";
+            this.btnDeleteUser.Size = new System.Drawing.Size(112, 23);
+            this.btnDeleteUser.TabIndex = 3;
+            this.btnDeleteUser.Text = "Usuń użytkownika";
+            this.btnDeleteUser.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchUser
+            // 
+            this.btnSearchUser.Location = new System.Drawing.Point(366, 261);
+            this.btnSearchUser.Name = "btnSearchUser";
+            this.btnSearchUser.Size = new System.Drawing.Size(112, 23);
+            this.btnSearchUser.TabIndex = 4;
+            this.btnSearchUser.Text = "Szukaj użytkownika";
+            this.btnSearchUser.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(366, 12);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(112, 23);
+            this.btnLogout.TabIndex = 5;
+            this.btnLogout.Text = "Wyloguj się";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 313);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnSearchUser);
+            this.Controls.Add(this.btnDeleteUser);
+            this.Controls.Add(this.btnEditUser);
+            this.Controls.Add(this.lstViewAllUsers);
+            this.Controls.Add(this.btnAddUser);
+            this.Name = "AdminForm";
+            this.Text = "Panel administratora";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.ListView lstViewAllUsers;
+        private System.Windows.Forms.Button btnEditUser;
+        private System.Windows.Forms.Button btnDeleteUser;
+        private System.Windows.Forms.Button btnSearchUser;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
